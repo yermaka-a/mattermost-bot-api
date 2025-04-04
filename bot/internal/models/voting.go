1f1fdbd8d5c8a07beb9dc895d65af4c37276ebcd
yermaka <ermak.080@mail.ru>
2025-04-05 05:02:39 +0800

    add voting struct - LogValue()

--- bot/internal/models/voting.go
+++ bot/internal/models/voting.go
@@ -1,5 +1,7 @@
 package models
 
+import "log/slog"
+
 type Voting struct {
 	ID        string   `msgpack:"id"`
 	CreatorID string   `msgpack:"creator_id"`
@@ -9,4 +11,14 @@ type Voting struct {
 	CreatedAt int64    `msgpack:"created_at"`
 	IsActive  bool     `msgpack:"is_active"`
 	ExpiresAt int64    `msgpack:"expires_at,omitempty"`
+}
+
+func (v *Voting) LogValue() slog.Value {
+	return slog.GroupValue(
+		slog.String("id", v.ID),
+		slog.String("creator_id", v.CreatorID),
+		slog.Int64("created_at", v.CreatedAt),
+		slog.Int64("expires_at", v.ExpiresAt),
+		slog.Bool("is_active", v.IsActive),
+	)
 }
